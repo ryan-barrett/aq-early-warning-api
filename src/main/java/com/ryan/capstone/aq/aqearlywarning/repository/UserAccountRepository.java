17e2b6b376c4443e689560a3f24417cb47c0640d
Ryan Barrett <ryan.barrett@spookyhouse.org>
2021-11-28 20:47:40 -0800

    clean up notification queries

--- src/main/java/com/ryan/capstone/aq/aqearlywarning/repository/UserAccountRepository.java
+++ src/main/java/com/ryan/capstone/aq/aqearlywarning/repository/UserAccountRepository.java
@@ -12,10 +12,25 @@ import java.time.LocalDateTime;
 
 @Repository
 public interface UserAccountRepository extends ReactiveCrudRepository<UserAccount, Integer> {
-    @Query("SELECT us.max_aqi, us.longitude, us.latitude, ua.id, ua.email, ua.first_name, ua.last_name, ua.apple_id, ua.is_safe FROM user_account AS ua INNER JOIN user_settings AS us on ua.id = us.user_id WHERE ((is_safe IS TRUE OR is_safe IS NULL) AND (last_checked < :earlierDate OR last_checked IS NULL)) AND us.max_aqi IS NOT NULL ORDER BY last_checked ASC LIMIT 3 FOR UPDATE")
+    @Query("SELECT us.max_aqi, us.longitude, us.latitude, ua.id, ua.email, ua.first_name, ua.last_name, ua.apple_id, ua.is_safe " +
+            "FROM user_account AS ua " +
+            "INNER JOIN user_settings AS us on ua.id = us.user_id " +
+            "WHERE ((is_safe IS TRUE OR is_safe IS NULL) AND (last_checked < :earlierDate OR last_checked IS NULL)) " +
+            "AND us.max_aqi IS NOT NULL " +
+            "AND (us.longitude IS NOT NULL AND us.latitude IS NOT NULL) " +
+            "ORDER BY last_checked ASC " +
+            "LIMIT 3 FOR UPDATE")
     Flux<UserDTO> findSafeUsersNeedingUpdate(LocalDateTime earlierDate);
 
-    @Query("SELECT us.max_aqi, us.longitude, us.latitude, ua.id, ua.email, ua.first_name, ua.last_name, ua.apple_id, ua.is_safe FROM user_account AS ua INNER JOIN user_settings AS us on ua.id = us.user_id WHERE ((is_safe IS FALSE) AND (last_checked < :earlierDate OR last_checked IS NULL)) AND us.max_aqi IS NOT NULL ORDER BY last_checked ASC LIMIT 3 FOR UPDATE")
+    @Query("SELECT us.max_aqi, us.longitude, us.latitude, ua.id, ua.email, ua.first_name, ua.last_name, ua.apple_id, ua.is_safe " +
+            "FROM user_account AS ua " +
+            "INNER JOIN user_settings AS us on ua.id = us.user_id " +
+            "WHERE ((is_safe IS FALSE) " +
+            "AND (last_checked < :earlierDate OR last_checked IS NULL)) " +
+            "AND us.max_aqi IS NOT NULL " +
+            "AND (us.longitude IS NOT NULL AND us.latitude IS NOT NULL) " +
+            "ORDER BY last_checked ASC " +
+            "LIMIT 3 FOR UPDATE")
     Flux<UserDTO> findUsersInDangerNeedingUpdate(LocalDateTime earlierDate);
 
     @Query("SELECT * FROM user_account where apple_id = :appleId")
