e532d2592477ac12ee00da22abfa4972ea1e5a98
Ryan Barrett <ryan.barrett@spookyhouse.org>
2021-12-01 19:18:24 -0800

    move spring config to config package

--- src/main/java/com/ryan/capstone/aq/aqearlywarning/AqEarlyWarningApplication.java
+++ src/main/java/com/ryan/capstone/aq/aqearlywarning/AqEarlyWarningApplication.java
@@ -7,9 +7,7 @@ import com.google.firebase.messaging.FirebaseMessaging;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 import java.io.IOException;
 
@@ -30,9 +28,4 @@ public class AqEarlyWarningApplication {
         FirebaseApp app = FirebaseApp.initializeApp(firebaseOptions, "my-app");
         return FirebaseMessaging.getInstance(app);
     }
-}
-
-@Configuration
-@EnableScheduling
-class SpringConfig {
 }
---
+++ src/main/java/com/ryan/capstone/aq/aqearlywarning/configuration/SpringConfig.java
@@ -0,0 +1,9 @@
+package com.ryan.capstone.aq.aqearlywarning.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@Configuration
+@EnableScheduling
+class SpringConfig {
+}
